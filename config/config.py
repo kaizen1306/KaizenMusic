bf513e91807482420d28101d3556b9c1062dbbbf
ZeebFly <zeebrobot@gmail.com>
2025-06-16 05:25:05 +0000

    Update config.py

--- config/config.py
+++ config/config.py
@@ -84,7 +84,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ZeebFly/MusicPlayer.git",
+    "https://github.com/ZeebFly/ZeebMusic.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
