88280c48d0339fd1e22cbcdf86c283fe9bd40c1f
ZeebFly <zeebrobot@gmail.com>
2025-06-16 12:37:35 +0700

    Update ping.py

--- ZeebMusic/plugins/tools/ping.py
+++ ZeebMusic/plugins/tools/ping.py
@@ -5,7 +5,7 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from ZeebMusic import app
-from ZeebMusic.core.call import Ryn
+from ZeebMusic.core.call import Zb
 from ZeebMusic.utils import bot_sys_stats
 from ZeebMusic.utils.decorators.language import language
 from ZeebMusic.utils.inline import supp_markup
@@ -17,7 +17,7 @@ async def ping_com(client, message: Message, _):
     response = await message.reply_text(_["ping_1"].format(app.mention),
     )
     start = datetime.now()
-    pytgping = await Ryn.ping()
+    pytgping = await Zb.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
