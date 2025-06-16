6f2943256bd44695bb35a7cf6d528935b68a4186
ZeebFly <zeebrobot@gmail.com>
2025-06-16 05:22:36 +0000

    Update play.py

--- ZeebMusic/utils/decorators/play.py
+++ ZeebMusic/utils/decorators/play.py
@@ -9,7 +9,7 @@ from pyrogram.errors import (
 )
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from config import PLAYLIST_IMG_URL, PRIVATE_BOT_MODE, MUST_JOIN
+from config import PLAYLIST_IMG_URL, PRIVATE_BOT_MODE
 from config import SUPPORT_GROUP as SUPPORT_CHAT
 from config import adminlist
 from strings import get_string
@@ -48,19 +48,7 @@ def PlayWrapper(command):
             )
             return await message.reply_text(
                 _["general_4"], reply_markup=upl
-            )
-        if MUST_JOIN:
-            try:
-                await app.get_chat_member(MUST_JOIN, message.from_user.id)
-            except UserNotParticipant:
-                sub = await app.export_chat_invite_link(MUST_JOIN)
-                kontol = InlineKeyboardMarkup(
-                    [
-                        [InlineKeyboardButton("📑 Gabung Dulu", url=sub)]
-                    ]
-                )
-                return await message.reply_text(_["force_sub"].format(message.from_user.mention), reply_markup=kontol)
-             
+            )     
         if await is_maintenance() is False:
             if message.from_user.id not in SUDOERS:
                 return await message.reply_text(
