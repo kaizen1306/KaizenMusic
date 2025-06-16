646dfc03f9f365b3d5a121751cf729dd2c48567f
ZeebFly <zeebrobot@gmail.com>
2025-06-16 13:02:08 +0700

    Update watcher.py

--- ZeebMusic/plugins/misc/watcher.py
+++ ZeebMusic/plugins/misc/watcher.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 from config import LOG_GROUP_ID
 from ZeebMusic import app
-from ZeebMusic.core.call import Ryn
+from ZeebMusic.core.call import Zb
 
 
 @app.on_message(filters.video_chat_started, group=20)
@@ -12,7 +12,7 @@ async def force_stop_stream(_, message: Message):
     try:
         if message.left_chat_member and not message.left_chat_member is None:
             if message.left_chat_member.id == (await get_assistant(message.chat.id)).id:
-                return await Ryn.force_stop_stream(message.chat.id)
-        await Ryn.force_stop_stream(message.chat.id)
+                return await Zb.force_stop_stream(message.chat.id)
+        await Zb.force_stop_stream(message.chat.id)
     except Exception as e:
         await app.send_message(LOG_GROUP_ID, f"error in wathcher.py error is {e}")
